8345d35b72c23cb23ceea94f3e5568a952200516
Paulo Cunha <p_cunha@apple.com>
2023-10-28 21:21:16 -0300

    Edit and Delete Implemented

--- src/pages/notebook/Notebook.tsx
+++ src/pages/notebook/Notebook.tsx
@@ -3,28 +3,78 @@ import Loading from "../../components/Loading/Loading";
 import NoteCard from "../../components/NoteCard/NoteCard";
 import { Note } from "../../types/Note";
 import "./Notebook.css";
-import { useQuery } from "react-query";
+import { useMutation, useQuery, useQueryClient } from "react-query";
 
 export default function Notebook() {
+  const queryClient = useQueryClient();
+
   const {
     data: notes,
     isFetching,
     isError,
   } = useQuery("notes", async () => {
-    const response = await fetch("http://localhost:5000/notes");
+    const response = await fetch("http://localhost:3001/notes");
     if (!response.ok) {
-      throw new Error("Erro ao carregar os dados da lista de contatos");
+      throw new Error("Erro ao carregar os dados da lista de notas");
     }
 
     return response.json();
   });
 
-  const handleDeleteNote = () => {
-    // Lógica para deleção aqui
+  const deleteNoteMutation = useMutation(
+    async (id: number) => {
+      const response = await fetch(`http://localhost:3001/notes/${id}`, {
+        method: "DELETE",
+      });
+      if (!response.ok) {
+        throw new Error("Falha ao deletar a nota");
+      }
+    },
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries("notes");
+      },
+    }
+  );
+
+  const handleDeleteNote = (note: Note) => {
+    deleteNoteMutation.mutate(note.id);
   };
 
-  const handleEditNote = () => {
-    // Lógica para edição aqui
+  const editContactMutation = useMutation(
+    async (editedNote: Note) => {
+      const response = await fetch(
+        `http://localhost:3001/notes/${editedNote.id}`,
+        {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(editedNote),
+        }
+      );
+      if (!response.ok) {
+        throw new Error("Falha ao editar a nota");
+      }
+      return response.json();
+    },
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries("notes");
+      },
+    }
+  );
+
+  const handleEditNote = (
+    note: Note,
+    newTitle: string,
+    newDescription: string
+  ) => {
+    editContactMutation.mutate({
+      ...note,
+      title: newTitle,
+      description: newDescription,
+    });
   };
 
   if (isFetching) {
@@ -44,8 +94,10 @@ export default function Notebook() {
             key={note.id}
             title={note.title}
             description={note.description}
-            handleDelete={handleDeleteNote}
-            handleEdit={handleEditNote}
+            handleDelete={() => handleDeleteNote(note)}
+            handleEdit={(newTitle, newDescription) =>
+              handleEditNote(note, newTitle, newDescription)
+            }
           />
         ))}
       </div>
