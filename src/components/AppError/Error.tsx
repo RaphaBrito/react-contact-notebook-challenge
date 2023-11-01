5e710857df2684ea0262f958dea64899097b6efb
hugo97s <hss@cin.ufpe.br>
2023-11-01 10:42:23 -0300

    added 404 image to error screen

--- src/components/AppError/Error.tsx
+++ src/components/AppError/Error.tsx
@@ -1,3 +1,11 @@
+import './Error.css';
+import errorImage from '../../assets/images/error-image.png';
+
 export default function AppError() {
-  return <h1>Que pena, algo de errado aconteceu :(</h1>;
+  return (
+    <div className="error-container">
+      <img src={errorImage} alt="Error" className="error-image" />
+      <h1>Que pena, algo de errado aconteceu...</h1>
+    </div>
+  );
 }
